ef4ef48d5b8af18a0b5dabb6ebba10fb1a9c0443
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2023-11-27 12:31:33 +0900

    Title: 수들의 합 최대화하기, Time: 3090ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 231127/수들의 합 최대화하기/max-sum-of-numbers.java
+++ 231127/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -4,8 +4,6 @@ public class Main {
     private static final Scanner sc = new Scanner(System.in);
     private static int n, answer = Integer.MIN_VALUE;
     private static int[][] arr;
-    private static boolean[] rowUsed;
-    private static boolean[] colUsed;
 
     public static void main(String[] args) {
         n = sc.nextInt();
@@ -14,31 +12,29 @@ public class Main {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
             }
-        }
-        rowUsed = new boolean[n];
-        colUsed = new boolean[n];
+        } // O(N^2)
 
-        recursive(0, 0);
+        recursive(0, 0, new boolean[n], new boolean[n]);
         System.out.print(answer);
     }
 
-    private static void recursive(int pickCount, int sum) {
+    private static void recursive(int pickCount, int sum, boolean[] rowUsed, boolean[] colUsed) {
         if (pickCount == n) {
             answer = Math.max(answer, sum);
             return;
         }
 
         for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (rowUsed[row] || colUsed[col]) {
-                    continue;
+            if (!rowUsed[row]) {
+                for (int col = 0; col < n; col++) {
+                    if (!colUsed[col]) {
+                        rowUsed[row] = true;
+                        colUsed[col] = true;
+                        recursive(pickCount + 1, sum + arr[row][col], rowUsed, colUsed);
+                        rowUsed[row] = false;
+                        colUsed[col] = false;
+                    }
                 }
-
-                rowUsed[row] = true;
-                colUsed[col] = true;
-                recursive(pickCount + 1, sum + arr[row][col]);
-                rowUsed[row] = false;
-                colUsed[col] = false;
             }
         }
     }
