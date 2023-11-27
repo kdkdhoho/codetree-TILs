9a886e9c2e11a2aaa3a89b367712994243227e44
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2023-11-27 12:17:59 +0900

    Title: 거꾸로 순열, Time: 496ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 231127/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static int n;
+    private static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) {
+        n = sc.nextInt();
+
+        recursive(new ArrayList<>(), new boolean[n + 1]);
+        System.out.print(answer);
+    }
+
+    private static void recursive(List<Integer> pickedNumbers, boolean[] used) {
+        if (pickedNumbers.size() == n) {
+            String result = pickedNumbers.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(" "));
+            answer.append(result).append(System.lineSeparator());
+            return;
+        }
+
+        for (int number = n; number >= 1; number--) {
+            if (!used[number]) {
+                pickedNumbers.add(number);
+                used[number] = true;
+
+                recursive(pickedNumbers, used);
+
+                used[number] = false;
+                pickedNumbers.remove(pickedNumbers.size() - 1);
+            }
+        }
+    }
+}
