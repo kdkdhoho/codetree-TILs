2756e81ea9b82df2ee3ff1000a5bfd470a9aaf7f
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2023-12-08 09:10:29 +0900

    Title: 나이트, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231208/나이트/knight-movements.java
+++ 231208/나이트/knight-movements.java
@@ -1,6 +1,11 @@
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
+/**
+ * 1. 시작점으로부터 8방향을 BFS 한다.
+ * 큐에 넣는 조건: 격자 내부, 거리가 기록되지 않음
+ */
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
     private static final int NOT_MARKED = -1;
@@ -10,7 +15,6 @@ public class Main {
     private static int n;
     private static int startRow, startCol, endRow, endCol;
     private static int[][] steps;
-    private static boolean canMarkNextStep = true;
 
     public static void main(String[] args) {
         n = sc.nextInt();
@@ -18,45 +22,43 @@ public class Main {
         startCol = sc.nextInt() - 1;
         endRow = sc.nextInt() - 1;
         endCol = sc.nextInt() - 1;
-
         steps = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(steps[i], NOT_MARKED);
-        }
-        steps[endRow][endCol] = 0;
 
-        while (steps[startRow][startCol] == NOT_MARKED && canMarkNextStep) {
-            markNextSteps();
+        bfs(startRow, startCol);
+
+        int answer = -1;
+        if (steps[endRow][endCol] != 0) {
+            answer = steps[endRow][endCol];
         }
-        System.out.println(steps[startRow][startRow]);
+        System.out.println(answer);
     }
 
-    private static void markNextSteps() {
-        boolean isMarked = false;
+    private static void bfs(int startRow, int startCol) {
+        Queue<Integer> rowQueue = new LinkedList<>();
+        Queue<Integer> colQueue = new LinkedList<>();
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (steps[row][col] != NOT_MARKED) {
-                    isMarked = mark(row, col, isMarked);
+        rowQueue.add(startRow);
+        colQueue.add(startCol);
+
+        while (!rowQueue.isEmpty()) {
+            int row = rowQueue.poll();
+            int col = colQueue.poll();
+
+            for (int d = 0; d < dRow.length; d++) {
+                int nextRow = row + dRow[d];
+                int nextCol = col + dCol[d];
+
+                if (inArray(nextRow, nextCol) && steps[nextRow][nextCol] == 0) {
+                    steps[nextRow][nextCol] = steps[row][col] + 1;
+                    rowQueue.add(nextRow);
+                    colQueue.add(nextCol);
+
+                    if (nextRow == endRow && nextCol == endCol) {
+                        return;
+                    }
                 }
             }
         }
-
-        canMarkNextStep = isMarked;
-    }
-
-    private static boolean mark(int row, int col, boolean isMarked) {
-        for (int d = 0; d < dRow.length; d++) {
-            int nextRow = row + dRow[d];
-            int nextCol = col + dCol[d];
-
-            if (inArray(nextRow, nextCol) && steps[nextRow][nextCol] == NOT_MARKED) {
-                steps[nextRow][nextCol] = steps[row][col] + 1;
-                isMarked = true;
-            }
-        }
-
-        return isMarked;
     }
 
     private static boolean inArray(int row, int col) {
