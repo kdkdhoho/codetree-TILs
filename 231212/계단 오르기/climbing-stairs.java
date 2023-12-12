571188c2d7592b1b6d3516c693d7628cfadb4b73
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2023-12-12 12:03:00 +0900

    Title: 계단 오르기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 231212/계단 오르기/climbing-stairs.java
+++ 231212/계단 오르기/climbing-stairs.java
@@ -2,27 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
+    private static final int MAX_N = 1000;
+    private static final int MOD = 10007;
+    private static final int[] dp = new int[MAX_N + 1];
+
+    static {
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+    }
 
     public static void main(String[] args) {
         int n = sc.nextInt();
-        int[] memo = new int[n + 1];
 
-        if (n == 2) {
-            System.out.println(1);
-            return;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
         }
-
-        memo[2] = 1;
-        memo[3] = 1;
-
-        for (int i = 4; i < memo.length; i++) {
-            memo[i] = (memo[i - 2] + memo[i - 3]) % 10_007;
-        }
-
-        int answer = 0;
-        if (memo[n] != 0) {
-            answer = memo[n];
-        }
-        System.out.println(answer);
+        System.out.println(dp[n]);
     }
 }
