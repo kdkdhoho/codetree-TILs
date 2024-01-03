6533677240791f8d52b108ef8318a5f86e48aada
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-01-03 09:40:54 +0900

    Title: 사각형 채우기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240103/사각형 채우기/rectangle-fill.java
+++ 240103/사각형 채우기/rectangle-fill.java
@@ -15,7 +15,7 @@ public class Main {
         dp[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
         }
         System.out.print(dp[n]);
     }
