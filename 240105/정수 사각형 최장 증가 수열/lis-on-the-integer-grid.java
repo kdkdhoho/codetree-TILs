791f8ced2e953dc2181e9d6f0de4918fb62fbd08
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-01-05 09:47:31 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1272ms, Memory: 50MB, Status: Wrong Answer - CodeTree

--- 240105/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240105/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -1,6 +1,6 @@
-import java.awt.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +8,9 @@ public class Main {
     private static final int[] dRow = {-1, 1, 0, 0};
     private static final int[] dCol = {0, 0, -1, 1};
 
-    private static int n, answer = Integer.MIN_VALUE;
-    private static int[][] arr;
+    private static int n;
+    private static int[][] arr, dp;
+    private static final List<Cell> cells = new ArrayList<>();
 
     public static void main(String[] args) {
         n = sc.nextInt();
@@ -22,49 +23,83 @@ public class Main {
 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
-                bfs(row, col);
+                cells.add(new Cell(arr[row][col], row, col));
+            }
+        }
+        Collections.sort(cells);
+
+        dp = new int[n][n];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                dp[row][col] = 1;
             }
         }
 
-        System.out.println(answer);
-    }
-
-    private static void bfs(int row, int col) {
-        Queue<Point> queue = new LinkedList<>();
-        Queue<Integer> distance = new LinkedList<>();
-        boolean[][] visited = new boolean[n][n];
-
-        queue.add(new Point(row, col));
-        distance.add(1);
-        visited[row][col] = true;
-
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            int currentDistance = distance.poll();
-
-            answer = Math.max(answer, currentDistance);
-
-            int startRow = point.x;
-            int startCol = point.y;
+        for (int i = 0; i < cells.size(); i++) {
+            Cell cell = cells.get(i);
+            int row = cell.row();
+            int col = cell.col();
 
             for (int d = 0; d < dRow.length; d++) {
-                int nextRow = startRow + dRow[d];
-                int nextCol = startCol + dCol[d];
+                int nextRow = row + dRow[d];
+                int nextCol = col + dCol[d];
 
-                if (inArray(nextRow, nextCol) && !visited[nextRow][nextCol] && isBigger(nextRow, nextCol, startRow, startCol)) {
-                    visited[nextRow][nextCol] = true;
-                    queue.add(new Point(nextRow, nextCol));
-                    distance.add(currentDistance + 1);
+                if (inArray(nextRow, nextCol) && isBigger(nextRow, nextCol, row, col)) {
+                    dp[nextRow][nextCol] = Math.max(dp[nextRow][nextCol], dp[nextRow][nextCol] + 1);
                 }
             }
         }
+
+        int answer = Integer.MIN_VALUE;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                answer = Math.max(answer, dp[row][col]);
+            }
+        }
+        System.out.println(answer);
     }
 
     private static boolean inArray(int row, int col) {
-        return row >= 0 && row < n && col >= 0 && col < n;
+        return row >= 0 && col >= 0 && row < n && col < n;
     }
 
     private static boolean isBigger(int targetRow, int targetCol, int sourceRow, int sourceCol) {
         return arr[targetRow][targetCol] > arr[sourceRow][sourceCol];
+    }
+}
+
+class Cell implements Comparable<Cell> {
+
+    private final int value;
+    private final int row;
+    private final int col;
+
+    public Cell(int value, int row, int col) {
+        this.value = value;
+        this.row = row;
+        this.col = col;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public int row() {
+        return row;
+    }
+
+    public int col() {
+        return col;
+    }
+
+    @Override
+    public int compareTo(Cell o) {
+        if (this.value == o.value) {
+            if (this.row == o.row) {
+                return this.col - o.col;
+            }
+            return this.row - o.row;
+        }
+        return this.value - o.value;
     }
 }
