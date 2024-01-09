5faa0022d6fc2028fd94dc9e4c12d06047b36e44
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-01-09 10:33:56 +0900

    Title: 최대 증가 부분 수열, Time: 209ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240109/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,37 @@
+import static java.lang.Math.max;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static int n;
+    private static int[] arr;
+
+    public static void main(String[] args) {
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = -1;
+        for (int i = 0; i < n; i++) {
+            answer = max(answer, dp[i]);
+        }
+        System.out.println(answer);
+    }
+}
