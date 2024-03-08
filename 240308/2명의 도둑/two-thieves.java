958cfd27ed2772bd370991977735773bf2ce3a4b
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-08 10:41:04 +0900

    Title: 2명의 도둑, Time: 324ms, Memory: 22MB, Status: Passed - CodeTree

--- 240308/2명의 도둑/two-thieves.java
+++ 240308/2명의 도둑/two-thieves.java
@@ -3,6 +3,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * 2명의 도둑이 N*N 크기 방에서 물건을 훔친다.
+ * 두 도둑은 각각 하나의 행을 정하고, 해당 행에 연속한 M개의 열에 있는 물건을 훔칠 수 있다.
+ * 두 도둑이 같은 행을 고를 수는 있지만, 그때 선택한 M개의 열이 겹쳐서는 안된다.
+ * <p>
+ * 각 칸에는 무게를 나타내는 숫자가 적혀있다.
+ * 도둑 각각이 들 수 있는 최대 무게가 C이기에 두 도둑 모두가 각각 훔친 물건의 합이 C를 넘으면 안된다.
+ * 만약 선택한 M개의 열의 물건들 골랐을 때 C를 넘는다면, 그 중에서도 적절히 골라 고른 물건의 무게의 합이 C를 넘지 않도록 해야 한다.
+ * <p>
+ * 각 물건으로부터 얻을 수 있는 가치는 '무게^2' 이다.
+ * <p>
+ * 이러한 조건들 속에서 두 도둑이 얻을 수 있는 가치의 최대값을 구하시오.
+ */
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
 
@@ -66,12 +79,12 @@ public class Main {
         int firstCol = cols.get(0);
         int secondCol = cols.get(1);
 
-        for (int col = firstCol; col <= firstCol + m; col++) {
+        for (int col = firstCol; col < firstCol + m; col++) {
             if (col == secondCol) {
                 return true;
             }
         }
-        for (int col = secondCol; col <= secondCol + m; col++) {
+        for (int col = secondCol; col < secondCol + m; col++) {
             if (col == firstCol) {
                 return true;
             }
@@ -125,6 +138,7 @@ public class Main {
             if (weight > c) {
                 return maxValue;
             }
+
             maxValue = Math.max(maxValue, value);
             return maxValue;
         }
