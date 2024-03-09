d39dbb80780b61c4667333bff0cd386745347ed8
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-09 10:11:49 +0900

    Title: 순위 경쟁2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240309/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+/**
+A랑 B가 게임한다.
+게임 점수가 바뀔 때마다 점수가 1등인 사람들을 모아서 명예의 전당에 이름을 올린다.
+
+점수 변동 관련 정보가 주어졌을 때, 명예의 전당에 올라간 사람의 조합이 총 몇 번 바뀌는지 출력하시오.
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = 0;
+        boolean[] winner = new boolean[2];
+
+        int scoreA = 0;
+        int scoreB = 0;
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String player = sc.next();
+            int score = sc.nextInt();
+
+            if (player.equals("A")) {
+                scoreA += score;
+            }
+            if (player.equals("B")) {
+                scoreB += score;
+            }
+
+            boolean[] newWinner = new boolean[2];
+            if (scoreA > scoreB) {
+                newWinner[0] = true;
+            }
+            if (scoreA < scoreB) {
+                newWinner[1] = true;
+            }
+            if (scoreA == scoreB) {
+                newWinner[0] = true;
+                newWinner[1] = true;
+            }
+
+            if (winner[0] != newWinner[0]) {
+                winner = newWinner;
+                answer++;
+                continue;
+            }
+            if (winner[1] != newWinner[1]) {
+                winner = newWinner;
+                answer++;
+                continue;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
