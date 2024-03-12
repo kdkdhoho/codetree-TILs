d7ec6a8ed356319da681478e6410ff40843ff685
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-12 21:10:08 +0900

    Title: LED 전구 버튼, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240312/LED 전구 버튼/led-bulb-button.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static int n, b;
+    private static int[] lights;
+
+    public static void main(String[] args) {
+        n = sc.nextInt();
+        b = sc.nextInt();
+
+        lights = new int[n];
+        for (int i = 0; i < n; i++) {
+            lights[i] = sc.nextInt();
+        }
+
+        for (int cnt = 0; cnt < b; cnt++) {
+            int[] newLights = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                int leftIndex = i - 1;
+                if (leftIndex < 0) {
+                    leftIndex = n - 1;
+                }
+
+                if (lights[leftIndex] == 1) {
+                    if (lights[i] == 1) {
+                        newLights[i] = 0;
+                    } else {
+                        newLights[i] = 1;
+                    }
+                } else {
+                    newLights[i] = lights[i];
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                lights[i] = newLights[i];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(lights[i]).append(System.lineSeparator());
+        }
+        System.out.print(sb);
+    }
+}
