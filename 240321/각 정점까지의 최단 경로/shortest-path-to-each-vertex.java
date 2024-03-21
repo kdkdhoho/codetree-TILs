2b7c4d0ff484e599ec1c6a818984175a69293dda
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-21 14:25:24 +0900

    Title: 각 정점까지의 최단 경로, Time: 2312ms, Memory: 175MB, Status: Passed - CodeTree

--- 240321/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
+++ 240321/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
@@ -1,102 +1,119 @@
-import static java.util.Comparator.comparingInt;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Node {
+    int index, dist;
+
+    public Node(int index, int dist) {
+        this.index = index;
+        this.dist = dist;
+    }
+};
+
+class Element implements Comparable<Element> {
+    int dist, index;
+
+    public Element(int dist, int index) {
+        this.dist = dist;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(Element e) {
+        return this.dist - e.dist;   // dist 기준 오름차순 정렬
+    }
+};
 
 public class Main {
-    private static final Scanner sc = new Scanner(System.in);
-    private static final int MAX_DISTANCE = 3000000;
+    public static final int MAX_N = 20000;
+
+    // 변수 선언
+    public static int n, m, k;
+    public static ArrayList<Node>[] graph = new ArrayList[MAX_N + 1];
+    public static PriorityQueue<Element> pq = new PriorityQueue<>();
+
+    public static int[] dist = new int[MAX_N + 1];
 
     public static void main(String[] args) {
-        int vertexCount = sc.nextInt();
-        int edgeCount = sc.nextInt();
-        int startVertex = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
 
-        ArrayList<Edge>[] graph = new ArrayList[vertexCount + 1];
+        // 그래프를 인접리스트로 표현합니다.
+        for(int i = 1; i <= n; i++)
+            graph[i] = new ArrayList<>();
 
-//        Map<Integer, List<Edge>> graph = new HashMap<>();
-        for (int v = 1; v <= vertexCount; v++) {
-            graph[v] = new ArrayList<>();
+        // 양방향 그래프이므로
+        // 양쪽에 추가해줘야함에 유의합니다.
+        while(m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+
+            graph[x].add(new Node(y, z));
+            graph[y].add(new Node(x, z));
         }
 
-        for (int i = 0; i < edgeCount; i++) {
-            int vertexA = sc.nextInt();
-            int vertexB = sc.nextInt();
-            int weight = sc.nextInt();
+        // 그래프에 있는 모든 노드들에 대해
+        // 초기값을 전부 아주 큰 값으로 설정
+        // INT_MAX 그 자체로 설정하면
+        // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
+        // 적당히 큰 숫자로 적어줘야함에 유의!
+        for(int i = 1; i <= n; i++)
+            dist[i] = (int)1e9;
 
-            graph[vertexA].add(new Edge(vertexB, weight));
-            graph[vertexB].add(new Edge(vertexA, weight));
+        // 시작위치에는 dist값을 0으로 설정
+        dist[k] = 0;
 
-//            List<Edge> edgesA = graph.get(vertexA);
-//            edgesA.add(new Edge(vertexB, weight));
-//            graph.put(vertexA, edgesA);
-//
-//            List<Edge> edgesB = graph.get(vertexB);
-//            edgesB.add(new Edge(vertexA, weight));
-//            graph.put(vertexB, edgesB);
-        }
+        // 우선순위 큐에 시작점을 넣어줍니다.
+        // 거리가 가까운 곳이 먼저 나와야 하며
+        // 해당 지점이 어디인지에 대한 정보도 필요하므로
+        // (거리, 정점 번호) 형태로 넣어줘야 합니다.
+        pq.add(new Element(0, k));
 
-        int[] dist = new int[vertexCount + 1];
-        Arrays.fill(dist, MAX_DISTANCE);
-        dist[startVertex] = 0;
+        // O(|E|log|V|) 다익스트라 코드
+        // 우선순위 큐에
+        // 원소가 남아있다면 계속 진행해줍니다.
+        while(!pq.isEmpty()) {
+            // 가장 거리가 가까운 정보를 받아온 뒤, 원소를 제거해줍니다.
+            int minDist = pq.peek().dist;
+            int minIndex = pq.peek().index;
+            pq.poll();
 
-        PriorityQueue<Element> pq = new PriorityQueue<>(comparingInt(e -> e.distance));
-        pq.add(new Element(startVertex, 0));
-
-        while (!pq.isEmpty()) {
-            Element element = pq.poll();
-            int minVertex = element.vertex;
-            int minDistance = element.distance;
-
-            if (minDistance != dist[minVertex]) {
+            // 우선순위 큐를 이용하면
+            // 같은 정점의 원소가 
+            // 여러 번 들어가는 문제가 발생할 수 있어
+            // minDist가 최신 dist[minIndex]값과 다르다면
+            // 계산할 필요 없이 패스해줍니다.
+            if(minDist != dist[minIndex])
                 continue;
-            }
 
-//            List<Edge> edges = graph.get(minVertex);
-            List<Edge> edges = graph[minVertex];
-            for (Edge edge : edges) {
-                int targetVertex = edge.vertex;
-                int targetWeight = edge.weight;
+            // 최솟값에 해당하는 정점에 연결된 간선들을 보며
+            // 시작점으로부터의 최단거리 값을 갱신해줍니다.
+            for(int j = 0; j < graph[minIndex].size(); j++) {
+                int targetIndex = graph[minIndex].get(j).index;
+                int targetDist = graph[minIndex].get(j).dist;
 
-                int newDist = minDistance + targetWeight;
-                if (newDist < dist[targetVertex]) {
-                    dist[targetVertex] = newDist;
-                    pq.add(new Element(targetVertex, newDist));
+                // 현재 위치에서 연결된 간선으로 가는 것이 더 작다면
+                int newDist = dist[minIndex] + targetDist;
+                if(dist[targetIndex] > newDist) {
+                    // 값을 갱신해주고, 우선순위 큐에 해당 정보를 넣어줍니다.
+                    dist[targetIndex] = newDist;
+                    pq.add(new Element(newDist, targetIndex));
                 }
             }
         }
 
-//        StringBuilder answer = new StringBuilder();
-        for (int i = 1; i <= vertexCount; i++) {
-            if (dist[i] == MAX_DISTANCE) {
-//                answer.append(-1).append("\n");
+        // 시작점(k번 정점)으로부터 각 지점까지의 최단거리 값을 출력합니다.
+        for(int i = 1; i <= n; i++) {
+            // 만약 도달이 불가능하다면 -1을 출력합니다.
+            if(dist[i] == (int)1e9)
                 System.out.println(-1);
-            } else {
-//                answer.append(dist[i]).append("\n");
+            else
                 System.out.println(dist[i]);
-            }
         }
-//        System.out.print(answer);
-    }
-}
-
-class Edge {
-    int vertex, weight;
-
-    public Edge(int vertex, int weight) {
-        this.vertex = vertex;
-        this.weight = weight;
-    }
-}
-
-class Element {
-    int vertex, distance;
-
-    public Element(int vertex, int distance) {
-        this.vertex = vertex;
-        this.distance = distance;
     }
 }
