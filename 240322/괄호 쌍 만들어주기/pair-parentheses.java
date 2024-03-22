8bc09a8c827b79ed2692a43c8aa30b10c6de7253
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-22 16:13:58 +0900

    Title: 괄호 쌍 만들어주기, Time: 3003ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

--- 240322/괄호 쌍 만들어주기/pair-parentheses.java
+++ 240322/괄호 쌍 만들어주기/pair-parentheses.java
@@ -4,6 +4,8 @@ public class Main {
     private static final Scanner sc = new Scanner(System.in);
 
     private static String s;
+    private static final List<Integer> openers = new ArrayList<>();
+    private static final List<Integer> closers = new ArrayList<>();
 
     public static void main(String[] args) {
         s = sc.nextLine();
@@ -11,26 +13,35 @@ public class Main {
         int[] arr = new int[s.length()];
         for (int i = 0; i < s.length() - 1; i++) {
             char c = s.charAt(i);
+
             if (c == '(' && c == s.charAt(i + 1)) {
-                arr[i] = 1;
+                openers.add(i);
                 continue;
             }
 
             if (c == ')' && c == s.charAt(i + 1)) {
-                arr[i] = -1;
+                closers.add(i);
             }
         }
 
         int cnt = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) {
-                for (int j = arr.length - 1; j > i; j--) {
-                    if (arr[j] == -1) {
-                        cnt++;
-                    }
+        for (int opener : openers) {
+            for (int closer : closers) {
+                if (opener < closer) {
+                    cnt++;
                 }
             }
         }
         System.out.print(cnt);
+    }
+}
+
+class Element {
+    String type;
+    int index;
+
+    public Element(String type, int index) {
+        this.type = type;
+        this.index = index;
     }
 }
