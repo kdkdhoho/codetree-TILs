434c281c5c4b5e2968befb2f42a12f75d9bd2ee2
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-22 13:59:43 +0900

    Title: 가장 많이 겹치는 구간, Time: 1474ms, Memory: 58MB, Status: Passed - CodeTree

--- 240322/가장 많이 겹치는 구간/section-with-maximum-overlap.java
+++ 240322/가장 많이 겹치는 구간/section-with-maximum-overlap.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,25 +9,39 @@ public class Main {
 
     public static void main(String[] args) {
         int n = sc.nextInt();
-        int[] checked = new int[MAX_NUMBER + 1];
+        List<Point> points = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
+            int start = sc.nextInt();
+            int end = sc.nextInt();
 
-            checked[s]++;
-            checked[e]--;
+            points.add(new Point(start, 1));
+            points.add(new Point(end, -1));
         }
 
-        int[] sum = new int[MAX_NUMBER + 1];
-        for (int i = 1; i <= MAX_NUMBER; i++) {
-            sum[i] = sum[i - 1] + checked[i];
-        }
+        Collections.sort(points);
 
         int answer = -1;
-        for (int i = 1; i <= MAX_NUMBER; i++) {
-            answer = Math.max(answer, sum[i]);
+        int cnt = 0;
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.get(i);
+            cnt += point.value;
+            answer = Math.max(answer, cnt);
         }
         System.out.println(answer);
+    }
+}
+
+class Point implements Comparable<Point> {
+    int x, value;
+
+    public Point(int x, int value) {
+        this.x = x;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        return this.x - o.x;
     }
 }
