469d5c74424a63fbecb2c75d7588d0d77767ee50
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-25 23:48:36 +0900

    Title: 처음 등장하는 위치, Time: 2509ms, Memory: 64MB, Status: Wrong Answer - CodeTree

--- 240325/처음 등장하는 위치/first-appearing-position.java
+++ 240325/처음 등장하는 위치/first-appearing-position.java
@@ -3,20 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = sc.nextInt();
-        }
-
         Map<Integer, Integer> map = new TreeMap<>();
         int index = 1;
-        for (int number : numbers) {
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+
             if (map.containsKey(number)) {
                 continue;
             }
-            map.put(number, index);
-            index++;
+
+            map.put(number, index++);
         }
 
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
