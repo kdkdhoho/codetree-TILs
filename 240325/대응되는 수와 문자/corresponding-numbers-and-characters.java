5ccad255457a5ec3568008df4cfba80d78f5032e
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-25 11:07:19 +0900

    Title: 대응되는 수와 문자, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240325/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 240325/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -4,21 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Map<String, String> map = new HashMap<>();
-
+        Map<String, Integer> strMap = new HashMap<>();
+        Map<Integer, String> intMap = new HashMap<>();
+        
         int n = sc.nextInt();
         int m = sc.nextInt();
         sc.nextLine();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= m; i++) {
             String s = sc.nextLine();
-            map.put(s, String.valueOf(i));
-            map.put(String.valueOf(i), s);
-        }
-
-        for (int i = 0; i < m; i++) {
-            String s = sc.nextLine();
-            System.out.println(map.get(s));
+            strMap.put(s, i);
+            intMap.put(i, s);
         }
     }
 }
