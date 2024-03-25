fb4ddba8e658bc832e856f6eca0a59dd8bc0246c
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-25 11:09:39 +0900

    Title: 대응되는 수와 문자, Time: 1564ms, Memory: 175MB, Status: Passed - CodeTree

--- 240325/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 240325/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder answer = new StringBuilder();
 
         Map<String, Integer> strMap = new HashMap<>();
         Map<Integer, String> intMap = new HashMap<>();
@@ -11,10 +12,23 @@ public class Main {
         int m = sc.nextInt();
         sc.nextLine();
 
-        for (int i = 1; i <= m; i++) {
+        for (int i = 1; i <= n; i++) {
             String s = sc.nextLine();
             strMap.put(s, i);
             intMap.put(i, s);
         }
+
+        for (int i = 0; i < m; i++) {
+            String s = sc.nextLine();
+
+            try {
+                int num = Integer.parseInt(s);
+                answer.append(intMap.get(num)).append("\n");
+            } catch (NumberFormatException e) {
+                answer.append(strMap.get(s)).append("\n");
+            }
+        }
+
+        System.out.print(answer);
     }
 }
