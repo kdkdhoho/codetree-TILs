7c709ab0b269a31874743adc73faf5a5c9221d74
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-26 18:45:16 +0900

    Title: 자리 바꾸기 2, Time: 3015ms, Memory: 104MB, Status: Time Limit Exceed - CodeTree

--- 240326/자리 바꾸기 2/changing-seats-2.java
+++ 240326/자리 바꾸기 2/changing-seats-2.java
@@ -1,10 +1,12 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static final Scanner sc = new Scanner(System.in);
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        int n = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        String[] nk = br.readLine().split(" ");
+        int n = Integer.parseInt(nk[0]);
         Map<Integer, Integer> seats = new HashMap<>(); // K 자리에 V 사람이 앉아있다.
         for (int i = 1; i <= n; i++) {
             seats.put(i, i);
@@ -17,16 +19,18 @@ public class Main {
             result.put(i, init);
         }
 
-        int k = sc.nextInt();
+        int k = Integer.parseInt(nk[1]);
         int[][] commands = new int[k][2]; // k개의 명령들
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < 2; j++) {
-                commands[i][j] = sc.nextInt();
-            }
+        for (int i = 0; i < k; i++) { // O(K)
+            String[] inputs = br.readLine().split(" ");
+            int a = Integer.parseInt(inputs[0]);
+            int b = Integer.parseInt(inputs[1]);
+            commands[i][0] = a;
+            commands[i][1] = b;
         }
 
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < k; j++) {
+            for (int j = 0; j < k; j++) { // O(K * 3)
                 int[] command = commands[j];
                 int seatA = command[0];
                 int seatB = command[1];
@@ -47,7 +51,7 @@ public class Main {
         }
 
         StringBuilder answer = new StringBuilder();
-        for (Map.Entry<Integer, HashSet<Integer>> entry : result.entrySet()) {
+        for (Map.Entry<Integer, HashSet<Integer>> entry : result.entrySet()) { // O(N)
             answer.append(entry.getValue().size()).append("\n");
         }
         System.out.print(answer);
