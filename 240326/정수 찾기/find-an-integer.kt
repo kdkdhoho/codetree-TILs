9eab056aedf730e2a5d7a39286e1a3e0dac9d826
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-26 17:48:54 +0900

    Title: 정수 찾기, Time: 1239ms, Memory: 56MB, Status: Passed - CodeTree

---
+++ 240326/정수 찾기/find-an-integer.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+val sc = Scanner(System.`in`)
+
+fun main() {
+    val n = sc.nextInt()
+    val set: HashSet<Int> = HashSet()
+    for (i in 1..n) {
+        set.add(sc.nextInt())
+    }
+
+    val m = sc.nextInt()
+    val answer = StringBuilder()
+    for (i in 1..m) {
+        val number = sc.nextInt()
+        when {
+            number in set -> answer.append(1)
+            else -> answer.append(0)
+        }
+        answer.append("\n")
+    }
+    print(answer)
+}
