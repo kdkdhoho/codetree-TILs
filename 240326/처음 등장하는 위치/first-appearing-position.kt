5950640087f24571d38e3bcc47512c9c43435933
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-26 15:25:04 +0900

    Title: 처음 등장하는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240326/처음 등장하는 위치/first-appearing-position.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val numbers = readLine()!!.split(" ").map { x -> x.toInt() }.toList()
+
+    val map: TreeMap<Int, Int> = TreeMap()
+
+    var index = 1
+    for (number in numbers) {
+        if (!map.containsKey(number)) {
+            map.put(number, index++)
+        }
+    }
+
+    val answer: StringBuilder = StringBuilder()
+    for ((number, index) in map) {
+        answer.append("$number $index\n")
+    }
+    print(answer)
+}
