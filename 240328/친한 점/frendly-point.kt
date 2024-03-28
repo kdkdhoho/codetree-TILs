3bdb3915418108df52e5453cd3cdbab25401ed51
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-28 13:54:44 +0900

    Title: 친한 점, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240328/친한 점/frendly-point.kt
@@ -0,0 +1,43 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+    val m = sc.nextInt()
+
+    val treeSet: TreeSet<Point> = TreeSet()
+
+    for (i in 1..n) {
+        val x = sc.nextInt()
+        val y = sc.nextInt()
+        treeSet.add(Point(x, y))
+    }
+
+    println(treeSet)
+
+    for (i in 1..m) {
+        val x = sc.nextInt()
+        val y = sc.nextInt()
+        val point = Point(x, y)
+
+        if (treeSet.ceiling(point) == null) {
+            println("-1 -1")
+        } else {
+            val result = treeSet.ceiling(point)
+        }
+    }
+}
+
+data class Point(
+    val x: Int,
+    val y: Int
+): Comparable<Point> {
+
+    override fun compareTo(o: Point): Int {
+        if (this.x == o.x) {
+            return this.y - o.y
+        }
+        return this.x - o.x
+    }
+}
