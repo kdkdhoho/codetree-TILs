27362d16f443d0da5dcd0d4e80926ac273535ea0
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-03-29 12:44:49 +0900

    Title: 마지막으로 남은 숫자, Time: 1064ms, Memory: 35MB, Status: Passed - CodeTree

--- 240329/마지막으로 남은 숫자/last-remaining-number.kt
+++ 240329/마지막으로 남은 숫자/last-remaining-number.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.math.*
 
 val sc = Scanner(System.`in`)
 
@@ -10,11 +11,11 @@ fun main() {
         pq.add(sc.nextInt())
     }
 
-    while (pq.count() >= 2) {
+    while (pq.size >= 2) {
         val num1 = pq.poll()
         val num2 = pq.poll()
 
-        val diff = Math.abs(num1 - num2)
+        val diff = abs(num1 - num2)
 
         when {
             diff != 0 -> pq.add(diff)
@@ -22,5 +23,9 @@ fun main() {
         }
     }
 
-    print(pq.poll())
+    if (pq.isEmpty()) {
+        print(-1)
+    } else {
+        print(pq.poll())
+    }
 }
