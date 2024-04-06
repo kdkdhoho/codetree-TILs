de78ee6787808cfcc8622ead9cd4566bb3383458
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-06 20:24:48 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 962ms, Memory: 34MB, Status: Passed - CodeTree

--- 240406/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.kt
+++ 240406/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.kt
@@ -3,43 +3,25 @@ import java.util.*
 val sc = Scanner(System.`in`)
 
 val n = sc.nextInt()
-val arr = IntArray(n)
+val arr = IntArray(n + 1)
 
 fun main() {
-    for (i in 0..n-1) {
+    for (i in 1..n) {
         arr[i] = sc.nextInt()
     }
 
-    var i = 0
-    var j = 0
-    var answer = 0
     val set = hashSetOf<Int>()
-
-    while (true) {
-        if (!set.contains(arr[j])) {
-            set.add(arr[j])
+    var answer = 0
+    var j = 0
+    for (i in 1..n) {
+        while (j + 1 <= n && !set.contains(arr[j + 1])) {
             j++
-            continue
-        }
-
-        answer = Math.max(answer, (j-1)-i+1)
-        while (true) {
-            if (i >= j || !set.contains(arr[j])) {
-                break
-            }
-
-            set.remove(arr[i])
-            i++
+            set.add(arr[j])
         }
 
         answer = Math.max(answer, j - i + 1)
-        
-        j++
-        if (j >= n) {
-            break
-        } else {
-            set.add(arr[j])
-        }
+
+        set.remove(arr[i])
     }
 
     print(answer)
