4b61d8f3d8244610f96e5bd6aa7d33e59afc46da
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-07 15:56:11 +0900

    Title: 정수 두 개의 합 2, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240407/정수 두 개의 합 2/sum-of-two-integers-2.kt
+++ 240407/정수 두 개의 합 2/sum-of-two-integers-2.kt
@@ -6,6 +6,13 @@ val n = sc.nextInt()
 val k = sc.nextInt()
 val nums = IntArray(n + 1)
 
+/**
+j를 0부터 시작해서 앞으로 증가한다.
+i != j && nums[j]를 더했을 때 sum > k가 되는 순간까지 증가한다.
+증가가 끝났다면 count한다.
+
+다음 i를 한 칸 앞으로 옮기며 sum -= nums[i]를 한다.
+*/
 fun main() {
     for (i in 1..n) {
         nums[i] = sc.nextInt()
@@ -16,15 +23,19 @@ fun main() {
     var j = 0
 
     for (i in 1..n) {
+        sum += nums[i]
+
         while (j + 1 <= n && sum + nums[j + 1] <= k) {
-            sum += nums[j + 1]
             j++
-        }
+            sum += nums[j]
 
-        if (i != j) {
-            answer++
-        }
+            if (i != j && sum <= k) {
+                answer++
+            }
 
+            sum -= nums[j]
+        }
+        sum += nums[j]
         sum -= nums[i]
     }
     print(answer)
