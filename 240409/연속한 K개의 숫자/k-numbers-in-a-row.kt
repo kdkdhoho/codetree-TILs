396c5c33bb4d706ec39f54f069e94357d8d97a59
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-09 14:24:58 +0900

    Title: 연속한 K개의 숫자, Time: 177ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/연속한 K개의 숫자/k-numbers-in-a-row.kt
@@ -0,0 +1,42 @@
+import java.util.*
+
+val sc = Scanner(System.`in`)
+
+val n = sc.nextInt()
+val k = sc.nextInt()
+val b = sc.nextInt()
+val arr = IntArray(n + 1)
+val emptyNumber = hashSetOf<Int>()
+val count = IntArray(n + 1)
+
+fun main() {
+    for (i in 1..b) {
+        emptyNumber.add(sc.nextInt())
+    }
+
+    for (i in 1..k) { // O(K)
+        when (emptyNumber.contains(i)) {
+            true -> count[i] = count[i - 1] + 1
+            else -> count[i] = count[i - 1]
+        }
+    }
+
+    for (i in k+1..n) { // O(K)
+        val s = i - k + 1
+
+        count[i] = count[i - 1]
+        
+        if (emptyNumber.contains(arr[s - 1])) {
+            count[i]--
+        }
+        if (emptyNumber.contains(arr[i])) {
+            count[i]++
+        }
+    }
+
+    var answer = Int.MAX_VALUE
+    for (i in 1..n) {
+        answer = Math.min(answer, count[i])
+    }
+    print(answer)
+}
