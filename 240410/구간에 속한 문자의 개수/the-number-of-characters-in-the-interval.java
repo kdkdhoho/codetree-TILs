4c44814fd424faf984c77cce3b6e80479608c6b3
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-10 12:09:40 +0900

    Title: 구간에 속한 문자의 개수, Time: 3187ms, Memory: 116MB, Status: Time Limit Exceed - CodeTree

--- 240410/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
+++ 240410/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
@@ -9,9 +9,9 @@ public class Main {
         sc.nextLine();
 
         char[][] arr = new char[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) { // O(N)
             String row = sc.nextLine();
-            for (int j = 0; j < row.length(); j++) {
+            for (int j = 0; j < row.length(); j++) { // O(M)
                 char c = row.charAt(j);
                 arr[i][j + 1] = c;
             }
@@ -19,14 +19,14 @@ public class Main {
 
         int[][][] pre = new int[n + 1][m + 1][3];
         for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
+            for (int j = 0; j <= m; j++) { // O(NM)
                 pre[i][j] = new int[3];
             }
         }
 
         // 누적 합 배열 생성
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = 1; j <= m; j++) { // O(10^6)
                 pre[i][j] = sub(add(pre[i - 1][j], pre[i][j - 1]), pre[i - 1][j - 1]);
                 if (arr[i][j] == 'a') {
                     pre[i][j][0]++;
@@ -39,14 +39,14 @@ public class Main {
         }
 
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) { // O(10^5)
             String[] split = sc.nextLine().split(" ");
             int r1 = Integer.parseInt(split[0]);
             int c1 = Integer.parseInt(split[1]);
             int r2 = Integer.parseInt(split[2]);
             int c2 = Integer.parseInt(split[3]);
 
-            int[] count = count(r1, c1, r2, c2, pre);
+            int[] count = add(sub(sub(pre[r2][c2], pre[r1 - 1][c2]), pre[r2][c1 - 1]), pre[r1 - 1][c1 - 1]);
             for (int j = 0; j < 3; j++) {
                 answer.append(count[j]).append(" ");
             }
@@ -70,9 +70,5 @@ public class Main {
             result[i] = source[i] - target[i];
         }
         return result;
-    }
-
-    private static int[] count(int r1, int c1, int r2, int c2, int[][][] pre) {
-        return add(sub(sub(pre[r2][c2], pre[r1 - 1][c2]), pre[r2][c1 - 1]), pre[r1 - 1][c1 - 1]);
     }
 }
