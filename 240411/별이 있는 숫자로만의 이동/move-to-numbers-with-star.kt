5d6d110dfe977d3caea73abd24e00ae0c37a630d
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-11 16:47:16 +0900

    Title: 별이 있는 숫자로만의 이동, Time: 1755ms, Memory: 51MB, Status: Passed - CodeTree

--- 240411/별이 있는 숫자로만의 이동/move-to-numbers-with-star.kt
+++ 240411/별이 있는 숫자로만의 이동/move-to-numbers-with-star.kt
@@ -1,78 +1,45 @@
 import java.util.*
 
 val sc = Scanner(System.`in`)
-
 val n = sc.nextInt()
 val k = sc.nextInt()
 val arr = Array(n + 1) { IntArray(n + 1) }
 val prefix = Array(n + 1) { IntArray(n + 1) }
 
 fun main() {
-    for (i in 1..n) {
-        for (j in 1..n) {
-            arr[i][j] = sc.nextInt()
+    for (row in 1..n) {
+        for (col in 1..n) {
+            arr[row][col] = sc.nextInt()
         }
     }
 
-    for (i in 1..n) {
-        for (j in 1..n) {
-            prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + arr[i][j]
+    for (row in 1..n) {
+        for (col in 1..n) {
+            prefix[row][col] = prefix[row-1][col] + prefix[row][col-1] - prefix[row-1][col-1] + arr[row][col]
         }
     }
 
     var answer = 0
-    for (r in 1..n) {
-        for (c in 1..n) { // O(N^2)
-            val sum = getTotalSum(r, c) // O(K)
+    for (row in 1..n) {
+        for (col in 1..n) {
+            var sum = 0
+
+            for (r in row-k..row+k) {
+                if (r in 1..n) {
+                    val gap = k - Math.abs(row - r)
+                    val startCol = if (col - gap < 1) 1 else (col - gap)
+                    val endCol = if (col + gap > n) n else (col + gap)
+
+                    sum += getSectionSum(r, startCol, r, endCol)
+                }
+            }
+
             answer = Math.max(answer, sum)
         }
     }
     print(answer)
 }
 
-fun getTotalSum(
-    r: Int,
-    c: Int
-): Int {
-    val r1 = if (r - k < 1) 1 else r - k
-    val c1 = if (c - k < 1) 1 else c - k
-    val r2 = if (r + k > n) n else r + k
-    val c2 = if (c + k > n) n else c + k
-    var result = getSum(r1, c1, r2, c2)
-
-    for (i in 0..k-1) { // O(K)
-        result -= getSum(r-k+i, c+1+i, r-k+i, c+k) // 1사분면
-        result -= getSum(r-k+i, c-k, r-k+i, c-1-i) // 2사분면
-        result -= getSum(r+k-i, c-k, r+k-i, c-1-i) // 3사분면
-        result -= getSum(r+k-i, c+1+i, r+k-i, c+k) // 4사분면
-    }
-
-    return result
+fun getSectionSum(r1: Int, c1: Int, r2: Int, c2: Int): Int {
+    return prefix[r2][c2] - prefix[r1-1][c2] - prefix[r2][c1-1] + prefix[r1-1][c1-1]
 }
-
-fun getSum(
-    r1: Int,
-    c1: Int,
-    r2: Int,
-    c2: Int
-): Int {
-    if (!inArray(r1, c1) && !inArray(r2, c2)) {
-        return 0
-    }
-
-    val startRow = reSizing(r1)
-    val startCol = reSizing(c1)
-    val endRow = reSizing(r2)
-    val endCol = reSizing(c2)
-
-    return prefix[endRow][endCol] - prefix[startRow - 1][endCol] - prefix[endRow][startCol - 1] + prefix[startRow - 1][startCol - 1]
-}
-
-fun inArray(r: Int, c: Int) = r in 1..n && c in 1..n
-
-fun reSizing(i: Int) =
-    when {
-        i < 1 -> 1
-        i > n -> n
-        else -> i
-    }
