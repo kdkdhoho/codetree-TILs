ebb48caed0a32797b6c9abf0d4381da70d298dfb
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-11 14:54:25 +0900

    Title: 별이 있는 숫자로만의 이동, Time: 214ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240411/별이 있는 숫자로만의 이동/move-to-numbers-with-star.kt
+++ 240411/별이 있는 숫자로만의 이동/move-to-numbers-with-star.kt
@@ -35,15 +35,61 @@ fun getTotalSum(r: Int, c: Int): Int {
     val c1 = if (c-k < 1) 1 else c-k
     val r2 = if (r+k > n) n else r+k
     val c2 = if (c+k > n) n else c+k
-
     var result = getSum(r1, c1, r2, c2)
+
     for (i in 0..k-1) {
         result -= getSum(r-k+i, c+1+i, r-k+i, c+k) // 1사분면
         result -= getSum(r-1-i, c-k, r-1-i, c-k+i) // 2사분면
         result -= getSum(r+1+i, c-k, r+1+i, c-k+i) // 3사분면
         result -= getSum(r+k-i, c+1+i, r+k-i, c+k) // 4사분면
     }
+
     return result
+
+    // for (i in 0..k-1) { // 1사분면
+    //     val startRow = r-k+i
+    //     val startCol = c+1+i
+    //     val endRow = r-k+i
+    //     val endCol = c+k
+
+    //     result -= getSum(startRow, startCol, endRow, endCol)
+
+    //     if (!inArray(startRow, startCol) || !inArray(endRow, endCol)) {
+    //         result -= 0
+    //     } else {
+    //         result -= getSum(startRow, startCol, endRow, endCol)
+    //     }
+    // }
+    // for (i in 0..k-1) { // 2사분면
+    //     val startRow = r-1-i
+    //     val startCol = c-k
+    //     val endRow = r-1-i
+    //     val endCol = c-k+i
+
+    //     if (!inArray(startRow, startCol) || !inArray(endRow, endCol)) {
+    //         result -= 0
+    //     } else {
+    //         result -= getSum(startRow, startCol, endRow, endCol)
+    //     }
+    // }
+    // for (i in 0..k-1) { // 3사분면
+    //     val startRow = r+1+i
+    //     val startCol = c-k
+    //     val endRow = r+1+i
+    //     val endCol = c-k+i
+
+    //     if (!inArray(startRow, startCol) || !inArray(endRow, endCol)) {
+    //         result -= 0
+    //     } else {
+    //         result -= getSum(startRow, startCol, endRow, endCol)
+    //     }
+    // }
+
+    // for (i in 0..k-1) {
+    //     result -= getSum(r+1+i, c-k, r+1+i, c-k+i) // 3사분면
+    //     result -= getSum(r+k-i, c+1+i, r+k-i, c+k) // 4사분면
+    // }
+    // return result
 }
 
 fun getSum(r1: Int, c1: Int, r2: Int, c2: Int): Int {
@@ -53,6 +99,6 @@ fun getSum(r1: Int, c1: Int, r2: Int, c2: Int): Int {
     return prefix[r2][c2] - prefix[r1-1][c2] - prefix[r2][c1-1] + prefix[r1-1][c1-1]
 }
 
-fun inArray(i: Int, j: Int): Boolean {
-    return i >= 1 && i <= n && j >= 1 && j <= n
+fun inArray(r: Int, c: Int): Boolean {
+    return r >= 1 && r <= n && c >= 1 && c <= n
 }
