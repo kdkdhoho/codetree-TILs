d588857237132df07f51b4e26b143b1d7eb2f5ad
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-16 17:03:27 +0900

    Title: 점 개수 세기, Time: 895ms, Memory: 39MB, Status: Wrong Answer - CodeTree

--- 240416/점 개수 세기/count-number-of-points.kt
+++ 240416/점 개수 세기/count-number-of-points.kt
@@ -1,12 +1,3 @@
-/**
-수직선 상 위에 서로 다른 위치에 N개의 점이 있다.
-Q개의 질의에 대해 각각 구간 내 점의 개수는?
-
-1 <= N, Q <= 10^5
--10^9 <= points[i] <= 10^9
--10^9 <= a <= b <= 10^9
-*/
-
 import java.util.*
 
 val sc = Scanner(System.`in`)
@@ -14,36 +5,38 @@ val sc = Scanner(System.`in`)
 val n = sc.nextInt()
 val q = sc.nextInt()
 val treeSet = TreeSet<Int>()
-val treeMap = TreeMap<Int, Int>()
+val hashMap = hashMapOf<Int, Int>()
 val answer = StringBuilder()
 
 fun main() {
-    for (i in 1..n) {
-        treeSet.add(sc.nextInt())
-    }
+    for (i in 1..n) { // O(N)
+        treeSet.add(sc.nextInt()) // O(logN)
+    } // O(NlogN)
 
     var count = 1
-    while (!treeSet.isEmpty()) {
-        val minNum = treeSet.pollFirst()
-        treeMap.put(minNum, count)
+    for (num in treeSet) { // O(N)
+        hashMap.put(num, count) // O(logN)
         count += 1
-    }
+    } // O(NlogN)
 
-    for (i in 1..q) {
+    for (i in 1..q) { // O(Q)
         val a = sc.nextInt()
         val b = sc.nextInt()
 
-        if (treeMap.ceilingKey(a) == null) {
+        val newA = treeSet.ceiling(a) ?: 0
+        val newB = treeSet.floor(b) ?: 0
+
+        if (newA == 0 || newA > newB) {
             answer.append(0).append("\n")
             continue
         }
 
-        val start = treeMap.get(treeMap.ceilingKey(a)) ?: 0
-        val end = treeMap.get(treeMap.floorKey(b)) ?: 0
+        val start = hashMap.get(newA)!! // O(logN)
+        val end = hashMap.get(newB)!! // O(logN)
 
         val result = end - start + 1
         answer.append(result).append("\n")
-    }
+    } // O(QlogN)
 
     print(answer)
 }
