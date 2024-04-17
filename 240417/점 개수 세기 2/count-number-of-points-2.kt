862f0b6e7f4b676fe5941cd2d0bd72268066cb12
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-17 10:37:18 +0900

    Title: 점 개수 세기 2, Time: 897ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240417/점 개수 세기 2/count-number-of-points-2.kt
+++ 240417/점 개수 세기 2/count-number-of-points-2.kt
@@ -5,10 +5,7 @@ val sc = Scanner(System.`in`)
 val n = sc.nextInt()
 val q = sc.nextInt()
 val treeSet = TreeSet<Point>()
-// val hashMap = hashMapOf<Point, Int>()
 val answer = StringBuilder()
-
-// val INF_POINT = Point(Int.MAX_VALUE, Int.MAX_VALUE)
 
 fun main() {
     for (i in 1..n) {
@@ -16,13 +13,7 @@ fun main() {
         treeSet.add(point)
     }
 
-    // var newPoint = 1
-    // for (point in treeSet) {
-    //     hashMap.put(point, newPoint)
-    //     newPoint += 1
-    // }
-
-    for (i in 1..q) {
+    for (i in 1..q) { // O(Q)
         val x1 = sc.nextInt()
         val y1 = sc.nextInt()
         val x2 = sc.nextInt()
@@ -30,26 +21,28 @@ fun main() {
         val p1 = Point(x1, y1)
         val p2 = Point(x2, y2)
 
-        val startPoint: Point? = treeSet.ceiling(p1)
-        val endPoint: Point? = treeSet.floor(p2)
-        if (startPoint == null || endPoint == null) {
-            answer.append(0).append("\n")
-            continue
-        } else {
-            var count = 0
-            var currentPoint: Point = startPoint
-            while (currentPoint != endPoint) {
-                if (y1 <= currentPoint.y && currentPoint.y <= y2) {
+        val startPoint: Point? = treeSet.ceiling(p1) // O(logN)
+        val endPoint: Point? = treeSet.floor(p2) // O(logN)
+
+        when {
+            startPoint == null || endPoint == null -> answer.append(0).append("\n")
+
+            else -> {
+                var count = 0
+                var currentPoint: Point = startPoint
+                while (currentPoint != endPoint) { // O(N)
+                    if (y1 <= currentPoint.y && currentPoint.y <= y2) {
+                        count++
+                    }
+                    currentPoint = treeSet.higher(currentPoint) ?: break
+                }
+
+                if (currentPoint != null && y1 <= currentPoint.y && currentPoint.y <= y2) {
                     count++
                 }
-                currentPoint = treeSet.higher(currentPoint) ?: break
+                
+                answer.append(count).append("\n")
             }
-
-            if (currentPoint != null && y1 <= currentPoint.y && currentPoint.y <= y2) {
-                count++
-            }
-            
-            answer.append(count).append("\n")
         }
     }
 
