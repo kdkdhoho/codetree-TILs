3b8925c4c568f1536ca189ddd39fd7329b467ff0
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-21 19:51:13 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 908ms, Memory: 44MB, Status: Wrong Answer - CodeTree

--- 240421/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.kt
+++ 240421/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.kt
@@ -5,7 +5,7 @@ val sc = Scanner(System.`in`)
 fun main() {
     val n = sc.nextInt()
     val nums = IntArray(n)
-    for (i in 0..n-1) {
+    for (i in 0..n - 1) {
         nums[i] = sc.nextInt()
     }
 
@@ -14,40 +14,62 @@ fun main() {
         return
     }
 
-    var lastIndex = -1
-    val dp = IntArray(n)
-    dp[0] = nums[0]
-    lastIndex = 0
+    val dp = Array(n) { TreeSet<Int>() }
+    dp[0].add(nums[0])
+    dp[1].add(nums[1])
+    dp[2].add(nums[0])
+    dp[2].add(nums[2])
 
-    if (nums[0] > nums[1]) {
-        dp[1] = nums[0]
-    } else {
-        lastIndex = 1
-        dp[1] = nums[1]
-    }
+    for (i in 3..n - 1) { // O(N)
+        val case1 = TreeSet(dp[i - 1])
+        val case2 = TreeSet(dp[i - 2])
+        val case3 = TreeSet(dp[i - 3])
 
-    for (i in 2..n-1) {
-        if (lastIndex == i - 1) {
-            if (dp[i - 1] < dp[i - 2] + nums[i]) {
-                dp[i] = dp[i - 2] + nums[i]
-                lastIndex = i
-            } else if (dp[i - 1] >= dp[i - 2] + nums[i]) {
-                dp[i] = dp[i - 1]
-            }
+        if (dp[i - 2].size < 3) {
+            case2.add(nums[i])
         } else {
-            if (dp[i - 2] + nums[i] < dp[i - 1] + nums[i]) {
-                dp[i] = dp[i - 1] + nums[i]
-                lastIndex = i
-            } else {
-                dp[i] = dp[i - 2] + nums[i]
-                lastIndex = i
+            if (dp[i - 2].first() < nums[i]) {
+                case2.pollFirst()
+                case2.add(nums[i])
             }
+        }
+
+        if (dp[i - 3].size < 3) {
+            case3.add(nums[i])
+        } else {
+            if (dp[i - 3].first() < nums[i]) {
+                case3.pollFirst()
+                case3.add(nums[i])
+            }
+        }
+
+        val a = getSum(case1)
+        val b = getSum(case2)
+        val c = getSum(case3)
+        val max = Math.max(a, Math.max(b, c))
+
+        when (max) {
+            getSum(case1) -> dp[i] = case1
+            getSum(case2) -> dp[i] = case2
+            getSum(case3) -> dp[i] = case3
+            else -> {}
         }
     }
 
     var answer = 0
-    for (i in 0..n-1) {
-        answer = Math.max(answer, dp[i])
+    for (i in 0..n - 1) {
+        if (dp[i].size == 3) {
+            val sum = getSum(dp[i])
+            answer = Math.max(answer, sum)
+        }
     }
     print(answer)
+}
+
+fun getSum(treeSet: TreeSet<Int>): Int {
+    var result = 0
+    for (num in treeSet) {
+        result += num
+    }
+    return result
 }
