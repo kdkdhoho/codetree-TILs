001a719affc19f71ff6dc9c367dfde07038919c7
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-22 14:19:13 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 786ms, Memory: 34MB, Status: Passed - CodeTree

--- 240422/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.kt
+++ 240422/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.kt
@@ -8,61 +8,23 @@ fun main() {
     for (i in 0..n-1) {
         nums[i] = sc.nextInt()
     }
-    
-    if (n == 5) {
-        print(nums[0] + nums[2] + nums[4])
-        return
+
+    val L = IntArray(n)
+    L[0] = nums[0]
+    for (i in 1..n-1) {
+        L[i] = Math.max(L[i - 1], nums[i])
     }
 
-    val dp = Array(n) { PriorityQueue<Int>() }
-    dp[0].add(nums[0])
-    dp[1].add(nums[1])
-    dp[2].add(nums[0])
-    dp[2].add(nums[2])
-
-    // println(dp.contentToString())
-
-    for (i in 3..n-1) {
-        val caseA = PriorityQueue(dp[i - 3])
-        val caseB = PriorityQueue(dp[i - 2])
-        val caseC = PriorityQueue(dp[i - 1])
-        caseA.add(nums[i])
-        caseB.add(nums[i])
-
-        val sumOfCaseA = getSum(caseA)
-        val sumOfCaseB = getSum(caseB)
-        val sumOfCaseC = getSum(caseC)
-
-        val maxSum = Math.max(sumOfCaseA, Math.max(sumOfCaseB, sumOfCaseC))
-
-        // println("caseA=$caseA caseB=$caseB caseC=$caseC")
-        // println("sumOfCaseA=$sumOfCaseA sumOfCaseB=$sumOfCaseB sumOfCaseC=$sumOfCaseC")
-        // println("maxSum=$maxSum")
-
-        when (maxSum) {
-            sumOfCaseC -> dp[i] = caseC
-            sumOfCaseB -> dp[i] = caseB
-            sumOfCaseA -> dp[i] = caseA
-            else -> { }
-        }
-        // println(dp.contentToString())
+    val R = IntArray(n)
+    R[n - 1] = nums[n - 1]
+    for (i in n-2 downTo 0) {
+        R[i] = Math.max(R[i + 1], nums[i])
     }
 
-    var answer = 0
-    for (i in 0..n-1) {
-        val treeSet = dp[i]
-        if (treeSet.size == 3) {
-            val sum = getSum(treeSet)
-            answer = Math.max(answer, sum)
-        }
+    var max = 0
+    for (i in 2..n-3) {
+        val value = nums[i] + L[i - 2] + R[i + 2]
+        max = Math.max(max, value)
     }
-    print(answer)
-}
-
-fun getSum(pq: PriorityQueue<Int>): Int {
-    var result = 0
-    for (num in pq) {
-        result += num
-    }
-    return result
+    print(max)
 }
