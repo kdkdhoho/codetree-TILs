a9dd3f4559198d43ba66579e0b8b7b74877c82b6
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-22 13:55:19 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240422/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.kt
@@ -0,0 +1,56 @@
+import java.util.*
+
+val sc = Scanner(System.`in`)
+val n = sc.nextInt()
+val nums = IntArray(n)
+
+fun main() {
+    for (i in 0..n-1) {
+        nums[i] = sc.nextInt()
+    }
+
+    val dp = Array(n) { PriorityQueue<Int>() }
+    dp[0].add(nums[0])
+    dp[1].add(nums[1])
+    dp[2].add(nums[0])
+    dp[2].add(nums[2])
+
+    for (i in 3..n-1) {
+        val caseA = PriorityQueue(dp[i - 3])
+        val caseB = PriorityQueue(dp[i - 2])
+        val caseC = PriorityQueue(dp[i - 1])
+
+        caseA.add(nums[i])
+        caseB.add(nums[i])
+
+        val sumOfCaseA = getSum(caseA)
+        val sumOfCaseB = getSum(caseB)
+        val sumOfCaseC = getSum(caseC)
+        val maxSum = Math.max(sumOfCaseA, Math.max(sumOfCaseB, sumOfCaseC))
+
+        when (maxSum) {
+            sumOfCaseC -> dp[i] = caseC
+            sumOfCaseB -> dp[i] = caseB
+            sumOfCaseA -> dp[i] = caseA
+            else -> { }
+        }
+    }
+
+    var answer = 0
+    for (i in 0..n-1) {
+        val treeSet = dp[i]
+        if (treeSet.size == 3) {
+            val sum = getSum(treeSet)
+            answer = Math.max(answer, sum)
+        }
+    }
+    print(answer)
+}
+
+fun getSum(pq: PriorityQueue<Int>): Int {
+    var result = 0
+    for (num in pq) {
+        result += num
+    }
+    return result
+}
