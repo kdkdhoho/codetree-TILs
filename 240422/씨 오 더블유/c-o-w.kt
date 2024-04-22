2ec70147425a551eb8a1f027895ddd54fd14ae9d
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-22 15:19:15 +0900

    Title: 씨 오 더블유, Time: 215ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240422/씨 오 더블유/c-o-w.kt
@@ -0,0 +1,38 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    sc.nextLine()
+    val s = sc.nextLine()
+
+    val L = IntArray(n)
+    if (s[0] == 'C') {
+        L[0] = 1
+    }
+    for (i in 1..n-1) {
+        L[i] = L[i - 1]
+        if (s[i] == 'C') {
+            L[i]++
+        }
+    }
+
+    val R = IntArray(n)
+    if (s[n - 1] == 'W') {
+        R[n - 1] = 1
+    }
+    for (i in n-2 downTo 0) {
+        R[i] = R[i + 1]
+        if (s[i] == 'W') {
+            R[i]++
+        }
+    }
+
+    var answer = 0
+    for (i in 1..n-2) {
+        if (s[i] == 'O') {
+            answer += L[i - 1] * R[i + 1]
+        }
+    }
+    print(answer)
+}
