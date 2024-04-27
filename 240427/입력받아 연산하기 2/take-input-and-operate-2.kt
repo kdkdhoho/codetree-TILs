6d9a4f67978a4fe1ffcb340bf22c729a4e33e08d
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-27 11:51:54 +0900

    Title: 입력받아 연산하기 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240427/입력받아 연산하기 2/take-input-and-operate-2.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+val sc = Scanner(System.`in`)
+
+fun main() {
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+
+    a += 894
+    b /= 10
+
+    println("$a")
+    print("$b")
+}
