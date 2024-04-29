9f9125458552d72b31f78c32a0cb4c66828b8cfb
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-29 13:21:56 +0900

    Title: 최소 에너지 비용, Time: 269ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240429/최소 에너지 비용/minimum-energy-cost.kt
+++ 240429/최소 에너지 비용/minimum-energy-cost.kt
@@ -15,15 +15,14 @@ fun main() {
         cost[i] = sc.nextInt()
     }
 
-    val L = IntArray(n) // L[i]: 0부터 i번 장소 중, 최소 비용
+    val L = IntArray(n) // L[i]: 0부터 i번 장소 중, 에너지 1을 채울 때 드는 최소 비용
     L[0] = cost[0]
     for (i in 1..n-1) {
         L[i] = min(L[i - 1], cost[i])
     }
 
     val result = IntArray(n) // result[i]: i-1에서 i로 이동할 때, '에너지 x 최소 비용'
-    result[1] = energy[0] * cost[0]
-    for (i in 2..n-1) {
+    for (i in 1..n-1) {
         result[i] = L[i - 1] * energy[i - 1]
     }
 
