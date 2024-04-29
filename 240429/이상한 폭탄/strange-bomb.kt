5b3f11062a792c52db33c4f2390e4bbd08d65824
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-29 11:37:30 +0900

    Title: 이상한 폭탄, Time: 3022ms, Memory: 176MB, Status: Time Limit Exceed - CodeTree

---
+++ 240429/이상한 폭탄/strange-bomb.kt
@@ -0,0 +1,29 @@
+import kotlin.math.max
+
+fun main() {
+    val n_k = readLine()!!.split(" ")
+    val n = n_k[0].toInt()
+    val k = n_k[1].toInt()
+
+    val arr = IntArray(n)
+    for (i in 0..n-1) {
+        arr[i] = readLine()!!.toInt()
+    }
+
+    val prefix = Array(n) { hashSetOf<Int>() }
+    for (i in n-2 downTo 0) {
+        prefix[i].addAll(prefix[i + 1])
+        if (i < n-1-k) {
+            prefix[i].remove(arr[i + k + 1])
+        }
+        prefix[i].add(arr[i + 1])
+    }
+
+    var answer = 0
+    for (i in 0..n-1-k) {
+        if (prefix[i].contains(arr[i])) {
+            answer = max(answer, arr[i])
+        }
+    }
+    print(answer)
+}
