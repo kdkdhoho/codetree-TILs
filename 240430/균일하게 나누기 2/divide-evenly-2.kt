a62a4870e5d7e126776b585ee281044fbf757897
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-04-30 11:26:44 +0900

    Title: 균일하게 나누기 2, Time: 307ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240430/균일하게 나누기 2/divide-evenly-2.kt
+++ 240430/균일하게 나누기 2/divide-evenly-2.kt
@@ -1,48 +1,38 @@
-import kotlin.math.*
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val n = readLine()!!.toInt()
-    val points = mutableListOf<Point>()
-    for (i in 1..n) {
+    val points = arrayOfNulls<Point>(n)
+    for (i in 0..n-1) {
         val values = readLine()!!.split(" ")
-        points.add(Point(values[0].toInt(), values[1].toInt()))
+        points[i] = Point(values[0].toInt(), values[1].toInt())
     }
-    points.sort()
 
     var answer = Int.MAX_VALUE
     for (point in points) {
-        val a = point.x + 1
-        val b = point.y + 1
-
-        // println("a=$a b=$b")
+        val a = point!!.x + 1
+        val b = point!!.y + 1
 
         val count = IntArray(5)
-
         for (p in points) {
             val area = getArea(p, a, b)
             count[area]++
         }
 
-        // println(count.contentToString())
-
         var m = 0
         for (i in 1..4) {
-            if (count[i] != 0) {
-                m = max(m, count[i])
-            }
+            m = max(m, count[i])
         }
         answer = min(answer, m)
-
-        // println("m=$m answer=$answer")
-        // println()
     }
 
     print(answer)
 }
 
-fun getArea(point: Point, a: Int, b: Int): Int {
-    val x = point.x
-    val y = point.y
+fun getArea(point: Point?, a: Int, b: Int): Int {
+    val x = point!!.x
+    val y = point!!.y
 
     if (x > a && y > b) {
         return 1
@@ -56,18 +46,10 @@ fun getArea(point: Point, a: Int, b: Int): Int {
     if (x > a && y < b) {
         return 4
     }
-    return 0
+    throw IllegalArgumentException()
 }
 
 data class Point(
     val x: Int,
     val y: Int
-): Comparable<Point> {
-
-    override fun compareTo(o: Point): Int {
-        if (this.x == o.x) {
-            return this.y - o.y
-        }
-        return this.x - o.x
-    }
-}
+)
