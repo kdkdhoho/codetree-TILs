2062866f16caabac7079d405f2b5f069a80d67ab
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-06-05 16:41:33 +0900

    Title: 정수 두 개의 합 2, Time: 187ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240605/정수 두 개의 합 2/sum-of-two-integers-2.kt
+++ 240605/정수 두 개의 합 2/sum-of-two-integers-2.kt
@@ -2,36 +2,24 @@ val nk = readLine()!!.trim().split(" ").map { it.toInt() }
 val n = nk[0]
 val k = nk[1]
 val arr = IntArray(n) { readLine()!!.toInt() }.sortedArray()
+var answer = 0
 
 fun main() {
     var i = 0
     var j = 1
-    var sum = arr[i] + arr[j]
-    var answer = 0
+    var sum = arr[0] + arr[1]
 
-    while (i < n) {
+    while (i < j) {
         if (sum <= k) {
-            if (i == j) {
-                j++
-                if (j >= n) {
-                    break
-                }
-                sum += arr[j]
-            }
-
             answer++
             sum -= arr[j]
             j++
-            if (j >= n) {
-                break
-            }
+            if (j >= n) break
             sum += arr[j]
         } else {
             sum -= arr[i]
             i++
-            if (i >= n) {
-                break
-            }
+            if (i >= n) break
             sum += arr[i]
         }
     }
