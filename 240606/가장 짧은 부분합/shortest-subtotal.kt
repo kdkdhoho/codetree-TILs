f5d3c1ec9f90e2f98231847fc45e6d3bbb9f5bed
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2024-06-06 17:49:23 +0900

    Title: 가장 짧은 부분합, Time: 521ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240606/가장 짧은 부분합/shortest-subtotal.kt
@@ -0,0 +1,27 @@
+import kotlin.math.min
+
+val ns = readLine()!!.trim().split(" ").map { it.toInt() }
+val n = ns[0]
+val s = ns[1]
+val arr = readLine()!!.trim().split(" ").map { it.toInt() }
+
+fun main() {
+    var i = 0
+    var j = 0
+    var sum = arr[0]
+    var answer = Int.MAX_VALUE
+
+    while (i < n) {
+        if (sum < s) {
+            j++
+            if (j >= n) break
+            sum += arr[j]
+        } else {
+            answer = min(answer, (j - i + 1))
+            sum -= arr[i]
+            i++
+        }
+    }
+
+    print(answer)
+}
